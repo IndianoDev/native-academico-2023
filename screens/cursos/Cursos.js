fdde2b25746bde206d05d7249f169d4ad60a8759
Juan Costa Indiano <juan75indiano@gmail.com>
2023-10-18 21:58:37 -0300

    excluir

--- screens/cursos/Cursos.js
+++ screens/cursos/Cursos.js
@@ -1,26 +1,49 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { useFocusEffect } from '@react-navigation/native'
-import React, { useEffect, useState } from 'react'
-import { ScrollView, View } from 'react-native'
-import { Button, Card, FAB, IconButton, MD3DarkTheme, Text } from 'react-native-paper'
+import React, { useState } from 'react'
+import { ScrollView,  } from 'react-native'
+import { Button, Card, Dialog, FAB, IconButton,  Portal, Text } from 'react-native-paper'
 
-const Cursos = ({ navigation }) => {
+const Cursos = ({ navigation  }) => {
+
 
   const [cursos, setCursos] = useState([])
+  const [idExcluir, setExcluir] =useState(0)
+
+  const [visible, setVisible] = React.useState(false);
+
+  const showDialog = () => setVisible(true);
+
+  const hideDialog = () => setVisible(false);
 
   useFocusEffect(
+
     React.useCallback(() => {
-
-      AsyncStorage.getItem('cursos').then(resultado => {
-
-        resultado = JSON.parse(resultado) || []
-
-        console.log(resultado)
-        setCursos(resultado)
-      })
-
+      carregarDados()
     }, [])
   );
+
+  function carregarDados() {
+    AsyncStorage.getItem('cursos').then(resultado => {
+      resultado = JSON.parse(resultado) || []
+      console.log(resultado)
+      setCursos(resultado)
+    })
+  }
+
+function confirmarExclusao(id){
+  setExcluir(id)
+  setVisible(true)
+  
+}
+
+  function excluir() {
+
+    cursos.splice(idExcluir, 1)
+    AsyncStorage.setItem('cursos', JSON.stringify(cursos))
+      carregarDados()
+      setVisible(false)
+  }
 
   return (
     <>
@@ -31,22 +54,39 @@ const Cursos = ({ navigation }) => {
           <Card key={i} mode='outlined' style={{ marginBottom: 10 }}>
             <Card.Content>
               <Text variant="titleLarge">{item.nome}</Text>
-              <Text variant="bodyMedium">Duração: {item.duracao} sem.</Text>
+              <Text variant="bodyMedium">Duração: {item.duracao}</Text>
               <Text variant="bodyMedium">Modalidade: {item.modalidade}</Text>
             </Card.Content>
             <Card.Actions>
-              <IconButton icon='pencil-outline' />
-              <IconButton icon='trash-can-outline' />
+              <IconButton icon='pencil-outline'
+                iconColor='blue' 
+                onPress={() => navigation.push('cursos-form', {id: i, cursos: item})}
+                ></IconButton>
+
+              <IconButton icon='trash-can-outline'
+                iconColor='red'
+              onPress={() => confirmarExclusao(i)}></IconButton>
             </Card.Actions>
           </Card>
         ))}
-
+  <Portal>
+          <Dialog visible={visible} onDismiss={hideDialog}>           
+            <Dialog.Content>
+              <Text variant="bodyMedium">Deseja realmente Excluir o registo?</Text>
+            </Dialog.Content>
+            <Dialog.Actions>
+              <Button onPress={excluir}>sim</Button>
+              <Button onPress={hideDialog}>não</Button>
+            </Dialog.Actions>
+          </Dialog>
+        </Portal>
 
       </ScrollView>
 
       <FAB
         icon="plus"
         size='small'
+        color='blue'
         style={{ position: 'absolute', right: 10, bottom: 10 }}
         onPress={() => navigation.push('cursos-form')}
       />
--- screens/cursos/CursosForm.js
+++ screens/cursos/CursosForm.js
@@ -4,9 +4,12 @@ import React, { useState } from 'react'
 import { ScrollView } from 'react-native'
 import { Button, Text, TextInput } from 'react-native-paper'
 
-const CursosForm = ({navigation}) => {
+const CursosForm = ({navigation, route}) => {
 
-  const [dados, setDados] = useState({})
+  const curso = route.params?.curso || {}
+  const id = route.params?.id  
+
+  const [dados, setDados] = useState(curso)
 
   function handleChange(valor, campo) {
     setDados({...dados, [campo]: valor })
@@ -18,7 +21,13 @@ const CursosForm = ({navigation}) => {
       
       const cursos = JSON.parse(resultado) || []
       
-      cursos.push(dados)
+      if(id >= 0){
+        cursos.splice(id, 1, dados)
+      }else{  
+        cursos.push(dados)
+      }
+
+ 
       console.log(cursos)
   
       AsyncStorage.setItem('cursos', JSON.stringify(cursos))
